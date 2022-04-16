25321b989e9d62cc3465823b632a7176114fa74a
Eric Coleman <ericcoleman9217@gmail.com>
2022-04-15 20:20:24 -0500

    added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,22 @@
-
+import React from 'react';
 
 const Footer = () => {
-
+	return (
+		<footer>
+			<ul className='flex flex-wrap w-auto justify-center py-2'>
+			<li className='mx-3'>
+					<a href="https://www.instagram.com/realview_aero/">
+						<img
+							src={require('../../assets/instagram-logo.png')}
+							alt="GitHub Logo"
+							className="h-10 opacity-100 hover:opacity-60"
+						/>
+					</a>
+				
+				</li>
+			</ul>
+		</footer>
+	);
 }
 
-export default Footer
+export default Footer;
